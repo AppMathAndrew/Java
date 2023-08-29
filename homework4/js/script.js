93d866a5a4f82a914c607a3315d4efa58aed6299
igrobomb <aandrew1711@gmail.com>
2023-08-29 18:03:00 +0300

    homework 4 update

--- homework4/js/script.js
+++ homework4/js/script.js
@@ -1,9 +1,34 @@
-function calculateFactorial(n) {
-    if (n != 1) {
-        return n *= calculateFactorial(n - 1);
-    } 
-    return 1;
-}
+// function calculateFactorial(n) {
+//     if (n != 1) {
+//         return n *= calculateFactorial(n - 1);
+//     }
+//     return 1;
+// }
 
-let result = calculateFactorial(2);
-console.log(result);
+// let result = calculateFactorial(2);
+// console.log(result);
+
+//task #2
+
+// function reverseString(str) {
+//     const arr = str.split('');
+//     let newstr = "";
+//     for (let i = arr.length - 1; i >= 0; i--) {
+//         newstr += arr[i];
+//     }
+//     console.log(newstr);
+// };
+
+// reverseString("word");
+
+//task 3
+
+// const textRef = document.querySelector('input[name="day"]');
+
+// const buttonRef = document.querySelector('button');
+
+// buttonRef.addEventListener('click', () => {
+//     curval = Number(textRef.value);
+//     console.log(curval + 10);
+// })
+
